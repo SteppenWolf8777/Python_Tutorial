07d5d4ee2d3967e7e437970b5d7c399e45e2cfb9
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-03-29 16:16:35 +0400

    learning

--- tests/test_python_basic.py
+++ tests/test_python_basic.py
@@ -15,3 +15,33 @@ if p_speed > v_speed:
     print('Петя')
 else:
     print('Вася')
+
+
+p_speed = float(input())
+v_speed = float(input())
+t_speed = float(input())
+if p_speed > v_speed and p_speed > t_speed:
+    print('Петя')
+elif t_speed > v_speed and t_speed > p_speed:
+    print('Толя')
+else:
+    print('Вася')
+
+
+year = int(input())
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("YES")
+else:
+    print("NO")
+
+
+number = int(input())
+a = number // 1000           # первая цифра
+b = (number // 100) % 10     # вторая цифра
+c = (number // 10) % 10      # третья цифра
+d = number % 10              # четвертая цифра
+if a == d and b == c:
+    print("YES")
+else:
+    print("NO")
