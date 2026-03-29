0089e3d3125c389b95dbbe007202976c9b138085
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-03-29 16:18:03 +0400

    learning_2

--- tests/test_python_basic.py
+++ tests/test_python_basic.py
@@ -45,3 +45,15 @@ if a == d and b == c:
     print("YES")
 else:
     print("NO")
+
+
+name1 = input()
+name2 = input()
+name3 = input()
+
+if name1 <= name2 and name1 <= name3:
+    print(name1)
+elif name2 <= name1 and name2 <= name3:
+    print(name2)
+else:
+    print(name3)
