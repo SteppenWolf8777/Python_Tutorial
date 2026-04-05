c0a1c120671f2e3e7617c31b5aeb73b591812728
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-04-05 15:14:19 +0400

    commit_2

--- tests/test_python_basic_2.py
+++ tests/test_python_basic_2.py
@@ -19,3 +19,24 @@ def my_function(fname):
 my_function("Emil")
 my_function("Tobias")
 my_function("Linus")
+
+
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
