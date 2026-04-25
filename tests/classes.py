bef51d3132613e7eb550fc6c0877f4a31b704f05
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-04-25 20:47:40 +0400

    classes.py

--- tests/classes.py
+++ tests/classes.py
@@ -129,3 +129,13 @@ print(p1.lastname)
 print(p2.lastname)
 
 
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
